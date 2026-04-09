d7c02afbd6809ecc117ee0a4f212a0bc1ab99778
GN-30 <gnsn1200@gmail.com>
2026-04-09 18:23:40 +0530

    gitignore

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://cashbook-4o20.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
